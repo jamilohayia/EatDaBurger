bbabace0b265a3305bce776c10603de6e0049f30
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-13 14:25:40 -0400

    edited the the schema and seed files

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -28,9 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.updateOne(
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
+  burger.updateOne("eaten=true", condition, function(result) {
     if (result.changedRows === 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
